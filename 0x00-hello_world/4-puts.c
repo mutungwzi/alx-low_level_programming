176a8a292cf96dbe1b6fcccce2b80dfb8ce62b57
mutungwzi <ngulubeportia6@gmail.com>
2022-09-08 16:39:13 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 /**
  * main-entry point
  *
- * Return:0 after printing
- *
+ * Description: using the main function
+ * this program prints "Programming ls like building a multilingual puzzle
+ * return: 0
  */
 int main(void)
 {
-  puts("\"Programming is like
- building a multilingual puzzle");
-  return(0);
-}   
+	puts("\"Programming ls like building a multilingual puzzle");
+	return (0);
+}	
