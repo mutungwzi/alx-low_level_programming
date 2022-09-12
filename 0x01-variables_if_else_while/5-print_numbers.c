5d196acc8e3df21d472f3c256f979c06cd92407f
mutungwzi <ngulubeportia6@gmail.com>
2022-09-12 16:47:14 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ /**
+  * main - Entry point
+  * Description: Print single digit num
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+int i;
+
+while (i >= 0 && i < 10)
+{
+printf("%d", i);
+i++;
+}
+printf("\n");
+
+return (0);
+}
