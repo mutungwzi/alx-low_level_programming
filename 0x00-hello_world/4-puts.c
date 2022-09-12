6455003a4718607cd17a4c1b085373d5116b3cab
mutungwzi <ngulubeportia6@gmail.com>
2022-09-12 15:42:58 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 /**
- * main-main block
- * Return:0
- */
+*main-Entry point
+*Description: Print "Programming is like building a multilingual puzzle"
+*Return:Always 0(success)
+*/
 int main(void)
 {
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
-} 
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
