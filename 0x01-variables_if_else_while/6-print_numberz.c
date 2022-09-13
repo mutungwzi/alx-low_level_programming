482b714c3d3f2a1d524c5a3ff0c9a8475e78d4ea
mutungwzi <ngulubeportia6@gmail.com>
2022-09-12 17:37:11 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print integers using putchar
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+int i;
+
+i = 48;
+while (i < 58)
+{
+putchar(i);
+i++;
+}
+putchar('\n');
+
+return (0);
+}
