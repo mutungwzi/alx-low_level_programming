07e18d5ce12aa7698e0934a79f09c82a722373cf
mutungwzi <ngulubeportia6@gmail.com>
2022-09-14 06:26:47 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void);
+
+int main(void)
+{
+char b;
+int times;
+
+
+for (times = 0; times <= 9; times++)
+{
+for (b = 'b'; b <= 'z'; b++)
+{
+putchar(b)
+
+}
+putchar('\n');
+}
+return (0);
+}
