57ff1a04488ea45be657de01ef8a428c695e676d
mutungwzi <ngulubeportia6@gmail.com>
2022-09-21 12:09:37 -0700

    0. strcat

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,22 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
- * void prototypes()
- * int prototypes()
- * char prototypes()
- */
-void reverse_array(int *a, int n);
-void print_number(int n);
-void print_buffer(char *b, int size);
-int _strcmp(char *sl, char *s2);
+
+#include <stdio.h>
+
+
 char *_strcat(char *dest, char *src);
 char *_strcat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
-char *string_toupper(char *);
+int _strcmp(char *s1, char *s2);
+char *string_toupper(char *s2);
+void reverse_array(int *a, int n);
 char *cap_string(char *);
-char *leet(char *);
-char *rot13(char *);
-char *infinite_add(char *nl, char *n2, char *r, int size_r);
-char *add_strings(char *nl, char *n2, char *r, int r_index);
+
 
 #endif /*MAIN_H*/
