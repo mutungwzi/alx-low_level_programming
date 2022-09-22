d4295dca6113df7a3bf1ab994af1eda86c55d0b2
mutungwzi <ngulubeportia6@gmail.com>
2022-09-22 04:14:31 -0700

    main.h

---
+++ 0x04-more_functions_nested_loops/my.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: void
+ */
+
+int main(void)
+{
+int a;
+
+a = 48;
+while (a <= 58)
+{
+putchar(a);
+a++;
+}
+putchar ('\n');
+
+return (0);
+}
