e43b44c57b2f748707a923d49dbd5ee4a3f6c224
mutungwzi <ngulubeportia6@gmail.com>
2022-09-25 10:41:55 -0700

    Update - task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main-main block
@@ -6,9 +6,8 @@
  */
 
 int main(void)
-
 {
-printf("with proper grammar,but the outcome is a piece of art,\n");
-
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	
+	return (0);
 }
